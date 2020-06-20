639bf24c45db9b40ceb6dcae313bcc4382178585
jmhe <1143024986@qq.com>
2020-06-20 20:28:09 +0800

    二分查找

--- 二分查找/Order-agnostic Binary Search (easy) .java
+++ 二分查找/Order-agnostic Binary Search (easy) .java
@@ -1,7 +1,10 @@
 /**
 Order-agnostic Binary Search (easy)
 
-Given a sorted array of numbers, find if a given number ‘key’ is present in the array. Though we know that the array is sorted, we don’t know if it’s sorted in ascending or descending order. You should assume that the array can have duplicates.
+Given a sorted array of numbers, find if a given number ‘key’ is present in the array. 
+Though we know that the array is sorted,
+we don’t know if it’s sorted in ascending or descending order. 
+You should assume that the array can have duplicates.
 
 Write a function to return the index of the ‘key’ if it is present in the array, otherwise return -1.
 
