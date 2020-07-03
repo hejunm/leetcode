0726aa2f35214d2bc879fb273d51408ce4fe226d
jmhe <1143024986@qq.com>
2020-07-03 22:32:12 +0800

    修改注释

--- MergeInterval/986.区间列表的交集.java
+++ MergeInterval/986.区间列表的交集.java
@@ -10,15 +10,16 @@ import java.util.List;
 // @lc code=start
 class Solution {
     public int[][] intervalIntersection(int[][] A, int[][] B) {
+        List<int[]> result = new ArrayList<>();
+
         int aIndex = 0;
         int bIndex = 0;
-
-        List<int[]> result = new ArrayList<>();
-        //判断是否存在交集
+        
         while(aIndex<A.length && bIndex<B.length){
             int[] a = A[aIndex];
             int[] b = B[bIndex];
             
+            //判断是否存在交集
             if( (b[0] <= a[0]  && a[0] <= b[1]) || (a[0] <= b[0]  && b[0] <= a[1])){
                 result.add(new int[]{Math.max(a[0], b[0]), Math.min(a[1], b[1])});
             }
